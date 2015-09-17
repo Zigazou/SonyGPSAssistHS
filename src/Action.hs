115fefd0cef14ec80d2ca60d4fddaec73c49a3a1
Frédéric BISSON <zigazou@free.fr>
2015-09-17 21:51:03 +0200

    Change Action for Step terminology Factorised httpGetString and httpGetBin Some refactoring

--- src/Action.hs
+++ src/Action.hs
@@ -9,8 +9,8 @@ Stability   : experimental
 Portability : POSIX
 -}
 module Action
-( actionIO
-, action
+( stepIO
+, step
 , sonyDirs
 , getSonyMounts
 , httpGetString
@@ -20,7 +20,7 @@ module Action
 ) where
 
 import Network.Curl ( URLString, curlGetString, curlGetString_
-                    , CurlCode(CurlOK)
+                    , CurlCode(CurlOK), CurlOption
                     )
 import System.Directory ( doesDirectoryExist, getPermissions, writable
                         , createDirectoryIfMissing
@@ -35,18 +35,21 @@ import System.Exit (exitFailure)
 import Constant (sonyBaseDir, sonyGPSDir, sonyGPSName)
 import MTab (getMTab, fsVfstype, fsFile)
 
+type Step a = Either String a
+type StepIO a = IO (Step a)
+
 {- |
-An action is a function which stops the program immediately in case of an
+An step is a function which stops the program immediately in case of an
 error. Running it prints a message and "OK" if everything went well. Otherwise
 it prints an error message and exit.
 
 The function must return an `Either String a` value. Left value is the error
 message.
 
-`actionIO` is for functions which works in the IO monad.
+`stepIO` is for functions which works in the IO monad.
 -}
-actionIO :: String -> IO (Either String a) -> IO a
-actionIO msg todo = do
+stepIO :: String -> StepIO a -> IO a
+stepIO msg todo = do
     putStr msg >> putStr "... " >> hFlush stdout
     result <- todo
     case result of
@@ -54,17 +57,17 @@ actionIO msg todo = do
          Right value   -> putStrLn "OK" >> return value
 
 {- |
-An action is a function which stops the program immediately in case of an
+An step is a function which stops the program immediately in case of an
 error. Running it prints a message and "OK" if everything went well. Otherwise
 it prints an error message and exit.
 
 The function must return an `Either String a` value. Left value is the error
 message.
 
-`action` is for pure functions.
+`step` is for pure functions.
 -}
-action :: String -> Either String a -> IO a
-action msg result = do
+step :: String -> Step a -> IO a
+step msg result = do
     putStr msg >> putStr "... " >> hFlush stdout
     case result of
          Left msgError -> putStrLn msgError >> exitFailure
@@ -81,14 +84,12 @@ sonyDirs dirs = filterM isItASonyDir dirs >>= filterM isWritable
         isWritable = liftM writable . getPermissions
 
 {- |
-An action which returns the list of mounted points which contains Sony
+An step which returns the list of mounted points which contains Sony
 directory structures which are writables.
 -}
-getSonyMounts :: IO (Either String [FilePath])
+getSonyMounts :: StepIO [FilePath]
 getSonyMounts = do
-    mtab <- getMTab
-
-    let vfats = filter ((== "vfat") . fsVfstype) mtab
+    vfats <- liftM (filter ((== "vfat") . fsVfstype)) getMTab
 
     sonyMounts <- sonyDirs (fsFile <$> vfats)
 
@@ -97,40 +98,44 @@ getSonyMounts = do
                 else Right sonyMounts
 
 {- |
-An action using Curl to retrieve a `String` given an URL.
+Helper function for httpGetString and httpGetBin.
 -}
-httpGetString :: URLString -> IO (Either String String)
-httpGetString url = do
-    (rc, value) <- curlGetString url []
+httpGet :: (URLString -> [CurlOption] -> IO (CurlCode, a))
+        -> URLString
+        -> StepIO a
+httpGet get url = do
+    (rc, value) <- get url []
     return $ case rc of
         CurlOK -> Right value
         code   -> Left ("Cannot download " ++ url ++ " (" ++ show code ++ ")")
 
 {- |
-An action using Curl to retrieve a `ByteString` given an URL.
+An step using Curl to retrieve a `String` given an URL.
 -}
-httpGetBin :: URLString -> IO (Either String B.ByteString)
-httpGetBin url = do
-    (rc, value) <- curlGetString_ url []
-    return $ case rc of
-        CurlOK -> Right value
-        code   -> Left ("Cannot download " ++ url ++ " (" ++ show code ++ ")")
+httpGetString :: URLString -> StepIO String
+httpGetString = httpGet curlGetString
 
 {- |
-An action which calculates the MD5 of a `ByteString` and compares it to
+An step using Curl to retrieve a `ByteString` given an URL.
+-}
+httpGetBin :: URLString -> StepIO B.ByteString
+httpGetBin = httpGet curlGetString_
+
+{- |
+An step which calculates the MD5 of a `ByteString` and compares it to
 another. The MD5 must be given in an hexadecimal string.
 -}
-verifyMD5 :: B.ByteString -> String -> Either String ()
+verifyMD5 :: B.ByteString -> String -> Step ()
 verifyMD5 bdata wanted
     | calculated /= wanted = Left "Incorrect MD5"
     | otherwise = Right ()
     where calculated = show $ md5 bdata
 
 {- |
-An action which writes a `ByteString` containing GPS data in a dedicated file
+An step which writes a `ByteString` containing GPS data in a dedicated file
 residing on a device of which the mount point is given in a `FilePath`.
 -}
-writeGPS :: FilePath -> B.ByteString -> IO (Either String ())
+writeGPS :: FilePath -> B.ByteString -> StepIO ()
 writeGPS dir bdata = do
     createDirectoryIfMissing True (dir </> sonyGPSDir)
     B.writeFile (dir </> sonyGPSDir </> sonyGPSName) bdata 
--- src/SonyGPSAssist.hs
+++ src/SonyGPSAssist.hs
@@ -15,25 +15,24 @@ import Control.Monad (forM_)
 
 import Constant (gpsDataUrl, gpsMD5Url)
 import Action ( getSonyMounts, httpGetBin, httpGetString, verifyMD5, writeGPS
-              , actionIO, action
+              , stepIO, step
               )
 
 main :: IO ()
 main = do
     -- Search for any writable Sony mounted storage device. 
-    sonyMounts <- actionIO "Analyze mounted filesystems" getSonyMounts
+    sonyMounts <- stepIO "Analyze mounted filesystems" getSonyMounts
 
     -- Download GPS data from Sony site using Curl
-    gpsData <- actionIO "Retrieve GPS data from Sony site"
-                        (httpGetBin gpsDataUrl)
+    gpsData <- stepIO "Retrieve GPS data from Sony site" (httpGetBin gpsDataUrl)
 
     -- Download corresponding MD5 file
-    gpsMD5raw <- actionIO "Retrieve MD5 for GPS data from Sony site"
-                          (httpGetString gpsMD5Url)
+    gpsMD5raw <- stepIO "Retrieve MD5 for GPS data from Sony site"
+                        (httpGetString gpsMD5Url)
 
     -- Verify the MD5 matches
-    action "Check MD5" (verifyMD5 gpsData $ take 32 gpsMD5raw)
+    step "Check MD5" (verifyMD5 gpsData $ take 32 gpsMD5raw)
 
     -- For any Sony mounted storage device found, copy the GPS data
-    forM_ sonyMounts $ \mDir -> actionIO ("Write GPS data on " ++ mDir)
-                                         (writeGPS mDir gpsData)
+    forM_ sonyMounts $ \mDir -> stepIO ("Write GPS data on " ++ mDir)
+                                       (writeGPS mDir gpsData)
